79a2e9b4db1c19b8e8b723215c278c66d032988c
Quentin Ponchon <ponchonquentin@gmail.com>
2021-11-26 15:46:01 +0100

    Auto stash before merge of "Groupe2" and "origin/Groupe2"

--- Voyage-back/src/main/java/model/Client.java
+++ Voyage-back/src/main/java/model/Client.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Entity;
 
