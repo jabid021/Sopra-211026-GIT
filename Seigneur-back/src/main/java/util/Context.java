e1fcbea909892dbcf710db7546df0656cd724267
Jordan <Jordanabid@gmail.com>
2021-12-08 16:23:16 +0100

    debut seigneur front

--- Seigneur-back/src/main/java/util/Context.java
+++ Seigneur-back/src/main/java/util/Context.java
@@ -3,9 +3,13 @@ package util;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import dao.IDAOPersonnage;
+import dao.jpa.DAOPersonnage;
+
 public class Context {
 
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("projetJpa");
+	private IDAOPersonnage daoP = new DAOPersonnage();
 	private static Context singleton=null;
 
 	private Context() {}
@@ -26,6 +30,15 @@ public class Context {
 	public void setEmf(EntityManagerFactory emf) {
 		this.emf = emf;
 	}
+
+	public IDAOPersonnage getDaoP() {
+		return daoP;
+	}
+
+	public void setDaoP(IDAOPersonnage daoP) {
+		this.daoP = daoP;
+	}
+	
 	
 	
 	
---
+++ Seigneur-frontJ/src/main/webapp/personnage.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<table>
+
+
+
+<%
+//List<Personnage> p = Context.
+for(int i=1;i<10;i++)
+{
+	out.println("<tr><td></td><td>toto</td></tr>");
+}
+%>
+</table>
+
+
+
+
+</body>
+</html>
+
+
+<script></script>
