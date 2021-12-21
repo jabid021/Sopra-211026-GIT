ba58190ffdb15739788c914fe1b8f0d00fe61d1e
Unknown <o.gozlan@ajc-ingenierie.fr>
2021-12-21 12:21:24 +0100

    spring mvc

---
+++ olivier/seigneur-front/src/main/java/controller/FirstController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FirstController {
+
+	public FirstController() {
+	}
+
+	// @RequestMapping(value = "/hello",method = RequestMethod.GET)
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(name = "prenom") String prenom,
+			Model model) {
+		model.addAttribute("prenom", prenom);
+		return "hello";
+	}
+}
