bea20a47ec0ecab95c128f17d035916419c9be89
Unknown <o.gozlan@ajc-ingenierie.fr>
2022-01-10 17:15:20 +0100

    binding

--- olivier/angular/formationAngular/src/app/test-personne/test-personne.component.ts
+++ olivier/angular/formationAngular/src/app/test-personne/test-personne.component.ts
@@ -2,16 +2,25 @@ import { Personne } from './../model/personne';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test-personne',
+  selector: 'test-personne',
   templateUrl: './test-personne.component.html',
   styleUrls: ['./test-personne.component.css'],
 })
 export class TestPersonneComponent implements OnInit {
+  message: string = 'hello';
+  // personne: Personne = new Personne('olivier', 'gozlan');
+  couleur: string = 'yellow';
+  personne = { prenom: 'olivier', nom: 'gozlan' };
+
   constructor() {}
 
-  ngOnInit(): void {
-    let olivier: Personne = new Personne('olivier');
-    olivier.prenom = 'oooooo';
-    console.log(olivier);
+  ngOnInit(): void {}
+
+  getPersonne(): Personne {
+    return new Personne('toto');
+  }
+
+  displayAlert() {
+    alert('une alert');
   }
 }
